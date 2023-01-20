0e1121e247d069b938f45b51630f6531428c71fd
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-01-20 21:21:19 +0100

    feature: flake app

--- flake.nix
+++ flake.nix
@@ -18,7 +18,14 @@
           checkInputs = [ pytest hypothesis ];
           checkPhase = ''python -m pytest'';
         });
-        packages.default = pkgs.python3Packages.callPackage hiro {};
+
+        packages.hiro-package = pkgs.python3Packages.callPackage hiro {};
+        packages.hiro = pkgs.python3Packages.toPythonApplication packages.hiro-package;
+        packages.default = packages.hiro-package;
+
+        apps.hiro = flake-utils.lib.mkApp { drv = packages.hiro; };
+        apps.default = apps.hiro;
+
         devShell = pkgs.mkShell {
           buildInputs = [
             (pkgs.python310.withPackages (ps: with ps; [
